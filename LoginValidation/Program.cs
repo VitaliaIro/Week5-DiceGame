15349b005d8a7365174496369d3f48ce1a858ae7
Õpilane 53KK <opilane53@edu.lan>
2022-03-23 18:40:36 +0200

    add new projects

---
+++ LoginValidation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnust ja salasõna;
+            //kui sisestatud kasutajatunnus on "admin" ja salasõna on "admin1234"
+            //siis konsoolis kuvatakse "Tere tulemast";
+            //muul juhul konsoolis kuvatakse "Vale kasutajatunnus või salasõna. Proovi uuesti.".
+            //kasutajal on 3 katset
+
+            int i = 0;
+
+            while (i < 3) 
+            {
+                Console.WriteLine("Sisesta kasutajatunnus:");
+                string userName = Console.ReadLine();
+                Console.WriteLine("Sisesta salasõna:");
+                string userPassword = Console.ReadLine();
+
+                if (userName != "admin" || userPassword != "admin1234")
+                {
+                    i++;
+                    Console.WriteLine($"Vale kasutajatunnus või salasõna. Proovi uuesti.{3 - i} katset on jäänud.");
+                }
+                else
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+                }
+
+
+              
+
+            }
+            
+        }
+    }
+}
